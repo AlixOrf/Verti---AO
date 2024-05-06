616848a4adfafec209cf03ca5c7cd84b39a100c4
AlixOrf <alix.orfeuvre@epitech.digital>
2024-05-06 20:31:47 +0200

    Partie Front finie (ou quasiment)

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import {Link} from "react-router-dom";
 export default function LoginPage (){
     return(
     <form className="login">
+        <h1>Se connecter</h1>
         <input type="text" placeholder="Identifiant"/>
         <input type="text" placeholder="Mot de Passe"/>
         <button>Se connecter</button>
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -3,17 +3,18 @@ import {Link} from "react-router-dom";
 export default function RegisterPage (){
     return(
     <form className="register">
+        <h1>Créer un compte</h1>
         <input type="text" placeholder="Adresse Mail"/>
         <input type="text" placeholder="Identifiant"/>
         <input type="text" placeholder="Mot de Passe"/>
         <input type="text" placeholder="Confirmer le Mot de Passe"/>
         <button>Créer un compte</button>
-        <nav>
+        <div>
             <Link to="/login">J'ai déjà un compte</Link>   
-        </nav>
-        <nav>
+        </div>
+        <div>
             <Link to="">J'accepte les conditions générales d'utilisation</Link>   
-        </nav>
+        </div>
     </form>
     );
 }
