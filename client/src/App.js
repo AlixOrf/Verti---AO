86c8e5b061acf6aa4a20c537ebb5bf83c5edb719
AlixOrf <alix.orfeuvre@epitech.digital>
2024-05-06 14:34:35 +0200

    début de la page d'accueil

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './images/logo-fond-vert.png';
+import './App.css';
+
+function App() {
+  return (
+
+    <main>
+
+      <header>
+
+        <a href="" className="logo">
+          <img src={logo} alt="Logo" />
+        </a>
+
+        <nav>
+          <a href="">Login</a>   
+        </nav>
+
+      </header>
+
+    </main>
+
+  );
+}
+
+export default App;
